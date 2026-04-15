0944c20a5cdc6ec483829584b1761091b5721121
aramrajski <aramrajski@gmail.com>
2026-04-15 17:22:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split(" ")
+    if len(parts) != 3 and parts[0] == "mv":
+        return
+    _, source, destination = parts
+    if not os.path.exists(source):
+        return
+
+    directory, filename = os.path.split(destination)
+
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+
+    try:
+        with open(source, "r") as src_file:
+            content = src_file.read()
+
+        with open(destination, "w") as dst_file:
+            dst_file.write(content)
+
+    except Exception as e:
+        print(f"Błąd podczas przenoszenia: {e}")
+
+    os.remove(source)
